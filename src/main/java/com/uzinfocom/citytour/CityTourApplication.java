e7ffbd6205b3971265b28e270799d397906d329e
shakhzodbek1997 <yuldoshov.shakhozd@gmail.com>
2026-09-23 16:18:26 +0500

    set up completed

---
+++ src/main/java/com/uzinfocom/citytour/CityTourApplication.java
@@ -0,0 +1,13 @@
+package com.uzinfocom.citytour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzinfocom/citytour/CityTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzinfocom.citytour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
